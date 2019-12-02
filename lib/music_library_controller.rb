3ea8d358cf91f704cbb4c3c057c97bed15d78400
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-02 23:52:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -81,5 +81,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip
+    if input >=1 and input <= Song.all.size
+      puts "Playing "
   end
 end
