a81a8a16d23a51588444144b3f79ba515a994a90
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-02 23:12:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,10 +24,10 @@ class MusicLibraryController
   end
   
   def list_songs
-    # l = Song.all.sort_by { |s| s.name }
-    # l.each_with_index do |x, i| 
-    #   puts "#{i + 1}. " + x.artist.name + " - " + x.name + " - " + x.genre.name
-    # end
+    l = Song.all.sort_by { |s| s.name }
+    l.each_with_index do |x, i| 
+      puts "#{i + 1}. " + x.artist.name + " - " + x.name + " - " + x.genre.name
+    end
 
   end
 end
